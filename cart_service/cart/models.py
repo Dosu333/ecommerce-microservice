65297181a8c916c7e862b46874ee3898b596f7b2
Dosu333 <vladamir1865@gmail.com>
2025-03-10 12:32:49 +0100

    Refactor cart models to add Cart and CartItem classes

--- cart_service/cart/models.py
+++ cart_service/cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Cart(models.Model):
+    user_id = models.UUIDField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product_id = models.UUIDField() 
+    quantity = models.PositiveIntegerField(default=1)
