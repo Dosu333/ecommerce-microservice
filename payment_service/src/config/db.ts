64b979e0aee482b4fc98c9b328a0e8b54f5e35be
Dosu333 <vladamir1865@gmail.com>
2025-03-14 22:15:34 +0100

    Refactor import statements in db.ts

--- payment_service/src/config/db.ts
+++ payment_service/src/config/db.ts
@@ -1,6 +1,4 @@
 import { Sequelize } from "sequelize";
-import fs from "fs"
-import path from "path";
 import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "./dotenv.config";
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
