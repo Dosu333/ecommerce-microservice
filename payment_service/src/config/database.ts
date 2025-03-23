fb9ec3982924edfbcc43b0c8d42df4d2e02a9094
Dosu333 <vladamir1865@gmail.com>
2025-03-23 23:05:38 +0100

    Refactor payment service to include Wallet and Escrow models

--- payment_service/src/config/database.ts
+++ payment_service/src/config/database.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from "./dotenv.config";
 import Payment from "../models/Payment";
+import Wallet from "../models/Wallet";
+import Escrow from "../models/Escrow";
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
     host: DB_HOST,
@@ -10,5 +12,7 @@ export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
 })
 
 Payment.initModel(sequelize)
+Wallet.initModel(sequelize)
+Escrow.initModel(sequelize)
 
-export { Payment }
+export { Payment, Wallet, Escrow }
